39b7a1ba28dae12884b57dfef9f2b332d691d489
Abdallah Samir <abdallah01097261247@gmail.com>
2026-06-20 12:46:54 +0300

    enhancements

--- common/src/main/java/com/chat/app/utils/ConfigurationProperties.java
+++ common/src/main/java/com/chat/app/utils/ConfigurationProperties.java
@@ -1,8 +1,14 @@
 package com.chat.app.utils;
 
+import java.util.Map;
+
 public enum ConfigurationProperties {
 
     ;
+
+    public final static String applicationName = "CHATTING-BRIDGE";
+    public final static Map<String, Object> envVariables = Map.of("appName", applicationName);
     public final static String SERVICE_NAME = "messageService";
     public final static short SERVER_PORT = 8080;
+
 }

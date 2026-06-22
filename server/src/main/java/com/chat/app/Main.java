edff615ac57f1463de92f38844bf1ae6ea48ee1e
Abdallah Samir <abdallah01097261247@gmail.com>
2026-06-22 19:40:03 +0300

    Adding Shutdown Hook to free all connection from connection pool

--- server/src/main/java/com/chat/app/Main.java
+++ server/src/main/java/com/chat/app/Main.java
@@ -1,6 +1,9 @@
 package com.chat.app;
 
+import com.chat.app.repository.config.DataSourceConfig;
 import com.chat.app.service.impl.MessageServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -10,21 +13,25 @@ import static com.chat.app.utils.ConfigurationProperties.SERVICE_NAME;
 
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class.getName());
+    public static void main(String[] args) throws InterruptedException {
 
-    public static void main(String[] args) {
-
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("JVM shutdowns!!!");
+        }));
         startServer();
     }
 
-    private static void startServer() {
+    private static void startServer() throws InterruptedException {
         try {
-            System.out.printf("Trying to start the server on port : %d\n", SERVER_PORT);
+            logger.info("Trying to start the server using port :{}", SERVER_PORT);
             var registry = LocateRegistry.createRegistry(SERVER_PORT);
             var messageServer = new MessageServiceImpl();
             registry.rebind(SERVICE_NAME, messageServer);
-            System.out.println("Server Started");
+            logger.info("Server Started");
+            Thread.currentThread().join();
         } catch (RemoteException e) {
-            System.out.println("Error: " + e.getMessage());
+            logger.error("There is exception when trying to start the server with message: {}", e.getMessage());
             throw new RuntimeException(e);
         }
     }
